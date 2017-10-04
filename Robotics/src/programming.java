13fe7567a6889f340a2a4cefb4bdeb5dc4de06f3
mhs <mhs@MHS-Science2-23>
2017-10-04 16:51:49 -0700

    yay

---
+++ Robotics/src/programming.java
@@ -0,0 +1,12 @@
+
+public class programming {
+	public void main(String[] args){
+		double hi = 1.0;
+		int you = 1;
+		boolean kappa = true;
+		String see = ""; 
+		char kk= 'a';
+		int test = 111;
+		
+	}
+}
